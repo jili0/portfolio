fb6ab9a9e3ef501812f81b974705b9cabedc02bd
Jing <jingli0319@gmail.com>
2024-01-23 17:09:32 +0100

    Change image folder into public folder

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,8 +1,9 @@
+
 const projects = [
   {
     id: 1,
     href: "https://jili0.github.io/react-project/",
-    imgSrc: "/portfolio/src/images/MoodPadImg.png",
+    imgSrc:"./public/images/MoodPadImg.png",
     title: "MoodPad",
     description:
       "This is a react blog app with backend feature, with the help of Firebase. Each user, after logging in, can create, read, update and write his or her posts. The user can also change the user name and the profile picture. The data will then be saved in Firebase and be synchronized no matter where the user log in account the next time. ",
@@ -10,7 +11,7 @@ const projects = [
   {
     id: 2,
     href: "https://jili0.github.io/to-do-list/",
-    imgSrc: "/portfolio/src/images/toDoList.png",
+    imgSrc: "./public/images/toDoList.png",
     title: "To-Do List",
     description: "A To-Do List written in vanilla Javascript.",
   },
