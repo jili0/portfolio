7b19386ecda5b49671397a93e3e6e5b468f8fccf
Jing <jingli0319@gmail.com>
2024-01-24 11:29:06 +0100

    Redesign project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,8 +4,8 @@ const ProjectCard = ({ href, imgSrc, title, description }) => {
   return (
     <div className="projectCard">
       <a href={href} target="_blank">
-        <img className="projectImg" src={imgSrc} />
-        <div className="projectInfo">
+        <img className="projectImg" src={imgSrc} alt={title} width="400" height="400"/>
+        <div className="projectInfoContainer">
           <p className="projectTitle headerFont">{title}</p>
           <p className="projectDescription">{description}</p>
         </div>
@@ -14,10 +14,4 @@ const ProjectCard = ({ href, imgSrc, title, description }) => {
   );
 };
 
-ProjectCard.defaultProps = {
-  href: "",
-  imgSrc: "https://picsum.photos/280/140",
-  title: "Default title",
-  description: "Default description",
-};
 export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,28 @@
 import "../stylesheets/projects.css";
 import Title from "./Title";
 import ProjectCard from "./ProjectCard";
-import projects from "../data/projects";
+import moodPad from '/images/moodPad.png'
+import toDoList from '/images/toDoList.png'
 
 const Projects = () => {
   return (
     <>
       <Title id="projects" title="My" subTitle="Projects" />
       <div id="projectsContainer">
-        {projects.map(({ id, imgSrc, href, title, description }) => (
-          <ProjectCard
-            key={id}
-            imgSrc={imgSrc}
-            href={href}
-            title={title}
-            description={description}
-          />
-        ))}
+        <ProjectCard
+          id="1"
+          href="https://jili0.github.io/mood-pad/"
+          imgSrc={moodPad}
+          title="MoodPad"
+          description="This is a react blog app with backend feature, with the help of Firebase. Each user, after logging in, can create, read, update and write his or her posts. The user can also change the user name and the profile picture. The data will then be saved in Firebase and be synchronized no matter where the user log in account the next time. "
+        />
+        <ProjectCard
+          id="2"
+          href="https://jili0.github.io/to-do-list/"
+          imgSrc={toDoList}
+          title="To-Do List"
+          description="A To-Do List  written in vanilla Javascript."
+        />
       </div>
     </>
   );
